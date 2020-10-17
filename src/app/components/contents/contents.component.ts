cde2243a7517cc24092d2b3e164955e1ee0fdbb3
4wangyu <4wangyu@gmail.com>
2020-10-17 19:08:33 +0800

    style contents

--- src/app/components/contents/contents.component.ts
+++ src/app/components/contents/contents.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contents',
   templateUrl: './contents.component.html',
-  styleUrls: ['./contents.component.scss']
+  styleUrls: ['./contents.component.scss'],
 })
 export class ContentsComponent implements OnInit {
+  contents = Array(20).fill({
+    title:
+      'AFL finals 2020 live updates: Wasteful Geelong Cats take the early edge against Brisbane Lions in preliminary final',
+    feed: 'Sydney Morning Herald - Latest News',
+    time: '22 minutes ago',
+    content: 'more info...',
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
