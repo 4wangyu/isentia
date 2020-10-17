0877b7f942bad0d8ecb0d3e06e32418c2e3675e1
4wangyu <4wangyu@gmail.com>
2020-10-17 17:09:07 +0800

    style feeds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
-import { FeedsComponent } from './components/feeds/feeds.component';
-import { ContentsComponent } from './components/contents/contents.component';
+import { AppComponent } from './app.component';
 import { AddFeedComponent } from './components/add-feed/add-feed.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { FeedsComponent } from './components/feeds/feeds.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedsComponent,
     ContentsComponent,
-    AddFeedComponent
+    AddFeedComponent,
   ],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot({}, {})
-  ],
+  imports: [BrowserModule, StoreModule.forRoot({}, {}), MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/feeds/feeds.component.ts
+++ src/app/components/feeds/feeds.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-feeds',
   templateUrl: './feeds.component.html',
-  styleUrls: ['./feeds.component.scss']
+  styleUrls: ['./feeds.component.scss'],
 })
 export class FeedsComponent implements OnInit {
+  feeds = Array(10).fill({
+    title: 'South China Morning Post',
+    unread: 10,
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
