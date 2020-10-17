45b32230ff03ff8d23292f71697f4f93e3b59741
4wangyu <4wangyu@gmail.com>
2020-10-17 16:00:49 +0800

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { FeedsComponent } from './components/feeds/feeds.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { AddFeedComponent } from './components/add-feed/add-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedsComponent,
+    ContentsComponent,
+    AddFeedComponent
   ],
   imports: [
     BrowserModule,
