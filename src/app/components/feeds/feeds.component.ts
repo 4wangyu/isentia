0a5f413428934d634826ec88b66dd833a1ad9d91
4wangyu <4wangyu@gmail.com>
2020-10-18 18:19:46 +0800

    add test new feed

--- src/app/components/feeds/feeds.component.ts
+++ src/app/components/feeds/feeds.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppComponent } from '../../app.component';
 import { Feed } from '../../app.model';
+import { contentsUpdate } from '../../state/state.actions';
 
 @Component({
   selector: 'app-feeds',
@@ -13,11 +14,31 @@ export class FeedsComponent implements OnInit {
   feeds$: Observable<Feed[]>;
 
   constructor(
-    store: Store<{ feeds: Feed[] }>,
+    private store: Store<{ feeds: Feed[] }>,
     @Inject(forwardRef(() => AppComponent)) public appComp: AppComponent
   ) {
     this.feeds$ = store.select('feeds');
   }
 
   ngOnInit(): void {}
+
+  testNewFeed(): void {
+    this.store.dispatch(
+      contentsUpdate({
+        contents: [
+          {
+            title: 'Breaking News!!!',
+            pubDate: new Date()
+              .toISOString()
+              .replace(/T/, ' ')
+              .replace(/\..+/, ''),
+            link: 'https://theannoyingsite.com/',
+            guid: new Date().toISOString(),
+            content: 'More Info......',
+            channel: 'Isentia',
+          },
+        ],
+      })
+    );
+  }
 }
